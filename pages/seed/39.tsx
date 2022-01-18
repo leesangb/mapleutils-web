5ebf286d957e24dc08fa97a7976822a7aced0dae
Sangbin Lee <leesangbin@outlook.com>
2022-01-18 23:48:29 +0100

    add seed 39 page

---
+++ pages/seed/39.tsx
@@ -0,0 +1,19 @@
+interface QuestionAnswer {
+    question: string;
+    choices: [string, string, string, string];
+    answer: 0 | 1 | 2 | 3;
+}
+
+interface Seed39Props {
+    data: QuestionAnswer[];
+}
+
+const Seed39 = (props: Seed39Props) => {
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default Seed39;
