d4a14dbd47c5dc8bb51a5f9f3308ecc764e10c55
Sangbin Lee <leesangbin@outlook.com>
2022-01-26 21:16:16 +0100

    add title card

---
+++ src/components/card/TitleCard.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import { useMemo } from 'react';
+
+interface TitleCardProps {
+    title: string;
+}
+
+const TitleCard = (props: TitleCardProps) => {
+    return useMemo(() => (
+        <Card elevation={0}
+              variant={'outlined'}
+              sx={(theme) => ({ marginBottom: theme.spacing(1) })}>
+            <CardContent>
+                <Typography variant={'h1'}>
+                    {props.title}
+                </Typography>
+            </CardContent>
+        </Card>
+    ), [props.title]);
+};
+
+export default TitleCard;
