59318f0f246dd41aab750760f4a2e02d9b1a1167
Sangbin Lee <leesangbin@outlook.com>
2022-01-28 02:10:57 +0100

    fix TitleCard styles

--- src/components/card/TitleCard.tsx
+++ src/components/card/TitleCard.tsx
@@ -3,20 +3,26 @@ import { useMemo } from 'react';
 
 interface TitleCardProps {
     title: string;
+    marginRight?: number;
 }
 
 const TitleCard = (props: TitleCardProps) => {
     return useMemo(() => (
         <Card elevation={0}
               variant={'outlined'}
-              sx={(theme) => ({ marginBottom: theme.spacing(1) })}>
+              sx={(theme) => ({
+                  marginBottom: theme.spacing(1),
+                  marginRight: props.marginRight
+                      ? theme.spacing(props.marginRight)
+                      : undefined,
+              })}>
             <CardContent>
                 <Typography variant={'h1'}>
                     {props.title}
                 </Typography>
             </CardContent>
         </Card>
-    ), [props.title]);
+    ), [props]);
 };
 
 export default TitleCard;
