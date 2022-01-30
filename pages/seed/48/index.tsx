ba42eff49b0f5d03709aeb003f1ac40f10159c02
Sangbin Lee <leesangbin@outlook.com>
2022-01-30 23:24:41 +0100

    add page seed 48

---
+++ pages/seed/48/index.tsx
@@ -0,0 +1,12 @@
+import { TitleCard } from '@components/card';
+
+const Seed48 = () => {
+
+    return (
+        <>
+            <TitleCard title={'시드 48층'} />
+        </>
+    )
+}
+
+export default Seed48
