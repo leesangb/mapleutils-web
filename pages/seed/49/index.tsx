36868ad96082bda75a3e515847fd17e09943f404
Sangbin Lee <leesangbin@outlook.com>
2022-02-04 01:19:44 +0100

    add location filter

--- pages/seed/49/index.tsx
+++ pages/seed/49/index.tsx
@@ -1,13 +1,14 @@
 import { Seo, SeoProps } from '@components/seo';
 import { MonsterCard, TitleCard } from '@components/card';
 import { seed49Data, SeedLocation, SeedMobData } from '@data/seed/49';
-import { Masonry as MuiMasonry } from '@mui/lab';
-import { Alert, Box, Button, Card, CardContent, Paper, Snackbar, Tooltip } from '@mui/material';
+import { Alert, Button, Card, CardContent, Collapse, Divider, Grid, Paper, Snackbar, Tooltip } from '@mui/material';
 import { styled } from '@mui/system';
 import NextImage from 'next/image';
 import { ChangeEvent, useCallback, useMemo, useState } from 'react';
 import { SearchBar } from '@components/input';
 import { isHangulMatching } from '@tools/string';
+import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from '@mui/icons-material';
+import { Masonry } from '@mui/lab';
 
 const seoProps: SeoProps = {
     title: '더 시드 49층',
@@ -88,6 +89,9 @@ const Content = (props: ContentProps) => {
 const Seed49 = (props: Seed49Props) => {
     const { data } = props;
     const [silhouette, setSilhouette] = useState(true);
+    const allLocations = useMemo(() => data.map(l => l.location).sort((a, b) => a.localeCompare(b)), [data]);
+    const [locations, setLocations] = useState(allLocations);
+    const [collapse, setCollapse] = useState(true);
 
     const [search, setSearch] = useState<string>('');
     const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
@@ -96,12 +100,13 @@ const Seed49 = (props: Seed49Props) => {
     const handleClear = () => setSearch('');
 
     const filtered = useMemo(() =>
-            data.flatMap(l => l.mobs.map(m => ({
-                ...m,
-                location: l.location,
-            }))).filter(m => isHangulMatching(search, m.location, m.name))
+            data.filter(l => locations.includes(l.location))
+                .flatMap(l => l.mobs.map(m => ({
+                    ...m,
+                    location: l.location,
+                }))).filter(m => isHangulMatching(search, m.location, m.name))
                 .sort((a, b) => a.name.localeCompare(b.name))
-        , [search]);
+        , [search, locations]);
 
     return (
         <>
@@ -115,20 +120,69 @@ const Seed49 = (props: Seed49Props) => {
                 <CardContent>
                     <SearchBar placeholder={'몬스터 또는 마을 검색 (예: 주황버섯, ㅎㄴㅅㄴ, ...) [Ctrl] + [F] 또는 [F3]으로 포커싱, 초성 검색 ✅'}
                                value={search} onChange={handleSearch} onClear={handleClear} />
-                    <Box textAlign={'right'}>
-                        <Button sx={theme => ({ marginTop: theme.spacing(1) })} disableElevation variant={'contained'}
+                    <Grid justifyContent={'space-between'} sx={theme => ({ marginTop: theme.spacing(1) })} container
+                          spacing={1}>
+                        <Grid item>
+                            <Button
+                                disableElevation
+                                variant={'contained'}
                                 onClick={() => setSilhouette(s => !s)}>
-                            {silhouette ? '실루엣 OFF' : '실루엣 ON'}
-                        </Button>
+                                {silhouette ? '실루엣 OFF' : '실루엣 ON'}
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button onClick={() => setCollapse(!collapse)}
+                                    endIcon={collapse
+                                        ? <KeyboardArrowUpRounded />
+                                        : <KeyboardArrowDownRounded />}>
+                                필터
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Collapse in={!collapse} timeout={'auto'} unmountOnExit>
+                        <Grid container sx={theme => ({ marginTop: theme.spacing(1) })} spacing={1}>
 
-                    </Box>
+                            <Grid item xs={12}>
+                                <Divider />
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    variant={'contained'}
+                                    disableElevation
+                                    onClick={() => setLocations(allLocations)}>전체 선택</Button>
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    variant={'contained'}
+                                    disableElevation
+                                    onClick={() => setLocations(allLocations.filter(l => !locations.includes(l)))}>반전</Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Divider />
+                            </Grid>
+
+                            {
+                                allLocations.map(location => <Grid key={location} item>
+                                    <Button
+                                        size={'small'}
+                                        disableElevation
+                                        variant={locations.includes(location) ? 'contained' : 'outlined'}
+                                        onClick={() => locations.includes(location)
+                                            ? setLocations(locations.filter(l => l !== location))
+                                            : setLocations([...locations, location])
+                                        }>
+                                        {location}</Button>
+                                </Grid>)
+                            }
+                        </Grid>
+                    </Collapse>
                 </CardContent>
             </Card>
-            <MuiMasonry spacing={1} columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}>
+            <Masonry spacing={1} columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}>
                 {
                     filtered.map(mob => <Content silhouette={silhouette} mob={mob} key={mob.name} />)
                 }
-            </MuiMasonry>
+            </Masonry>
 
 
         </>
