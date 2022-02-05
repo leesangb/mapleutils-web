ad7760adba64bdef48c9d6084f5cdc93ff785e1b
Sangbin Lee <leesangbin@outlook.com>
2022-02-05 13:22:56 +0100

    add kakaotalk icon

---
+++ src/components/icons/KakaoTalkIcon.tsx
@@ -0,0 +1,15 @@
+import { SvgIcon } from '@mui/material';
+
+const KakaoTalkIcon = () => {
+
+    return (
+        <SvgIcon>
+            <path
+                transform='translate(3 3)'
+                d='M9.375 1.5C5.025 1.5 1.5 4.393 1.5 7.962c0 2.718 1.151 3.858 2.949 5.042l.01 3.315c0 .149.178.234.303.145L7.827 14.3c.5.082 1.018.125 1.548.125 4.35 0 7.875-2.893 7.875-6.463 0-3.569-3.526-6.462-7.875-6.462'
+            />
+        </SvgIcon>
+    );
+};
+
+export default KakaoTalkIcon;
