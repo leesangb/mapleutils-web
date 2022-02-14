12d2fcece56b1a55aabffe425f51fc0dc86a80ad
Sangbin Lee <leesangbin@outlook.com>
2022-02-14 02:53:50 +0100

    add character card

---
+++ src/components/card/CharacterCard.tsx
@@ -0,0 +1,64 @@
+import { Box, Card, CardActionArea, CardContent, Chip, Grid, Typography } from '@mui/material';
+import { Link } from '@components/link';
+import { styled } from '@mui/system';
+import NextImage from 'next/image';
+
+
+const StyledImg = styled(NextImage)(() => ({
+    transition: '0.2s',
+    pointerEvents: 'none',
+}));
+
+
+interface CharacterCardProps {
+    name: string;
+    server: string;
+    job: string;
+}
+
+const CharacterCard = ({ name, server, job }: CharacterCardProps) => {
+    const imageSrc = `/images/characters/${name}.png`;
+    const maplegg = `https://maple.gg/u/${name}`;
+    return (
+        <Card variant={'outlined'}>
+            <CardActionArea component={Link} href={maplegg} target={'_blank'} rel={'noopener noreferrer'}>
+                <Box sx={theme => ({
+                    padding: theme.spacing(1),
+                })}>
+                    <Card elevation={0}
+                          variant={'outlined'}
+                          sx={theme => ({
+                              marginBottom: theme.spacing(1),
+                              backgroundColor: theme.palette.background.default,
+                          })}>
+                        <Grid direction={'column'} container
+                              sx={theme => ({ position: 'absolute', margin: theme.spacing(0) })} spacing={1}>
+                            <Grid item>
+                                <Chip size={'small'} label={server} />
+                            </Grid>
+                            <Grid item>
+                                <Chip size={'small'} label={job} />
+                            </Grid>
+                        </Grid>
+                        <CardContent>
+                            <Box sx={theme => ({
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                marginTop: theme.spacing(1),
+                                marginBottom: theme.spacing(1),
+                                position: 'relative',
+                            })}>
+                                <StyledImg src={imageSrc} width={96} height={96} alt={name} />
+                            </Box>
+                        </CardContent>
+                    </Card>
+
+                    <Typography fontWeight={'medium'} align={'center'}>{name}</Typography>
+                </Box>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default CharacterCard;
--- src/components/card/index.ts
+++ src/components/card/index.ts
@@ -1,2 +1,3 @@
 export { default as TitleCard } from './TitleCard';
 export { default as MonsterCard } from './MonsterCard';
+export { default as CharacterCard } from './CharacterCard';
