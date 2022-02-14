2c639e3d96421822362a77076e4d7730c7a2241f
Sangbin Lee <leesangbin@outlook.com>
2022-02-15 00:13:12 +0100

    add image src for mobs

--- data/farm/mobs.ts
+++ data/farm/mobs.ts
@@ -8,20 +8,24 @@ export interface MonsterLifeMob {
     effect: string;
     grade: MonsterLifeGrade;
     other?: string;
+    img: string;
 }
 
 const fixMob = (mob: MonsterLifeMob): MonsterLifeMob => {
+    if (mob.category === '스페셜') {
+        return { ...mob, img: encodeURI(`/images/monster-life/${mob.name}.png`) };
+    }
     const ce = getFarmCategoryEffect(mob.category);
     const multiplier = getGradeMultiplier(mob.grade);
     const effect = `${ce.text} ${ce.suffix && ce.suffix.length > 1 ? '' : '+'}${ce.value * multiplier}${
         ce.suffix || ''
     }`;
-    return { ...mob, effect };
+    return { ...mob, effect, img: encodeURI(`/images/monster-life/${mob.name}.png`) };
 };
 
 
 const MobData: MonsterLifeMob[] = mobs as MonsterLifeMob[];
 
-export const monsterLifeMobs = MobData.map((mob) => (mob.category === '스페셜' ? mob : fixMob(mob)));
+export const monsterLifeMobs = MobData.map((mob) => fixMob(mob));
 
 export const getMonsterLifeMob = (name: string): MonsterLifeMob | undefined => monsterLifeMobs.find((m) => m.name === name);
--- data/seed/49/index.ts
+++ data/seed/49/index.ts
@@ -4,6 +4,7 @@ export interface SeedMobData {
     name: string;
     width: number;
     height: number;
+    img: string;
 }
 
 export interface SeedLocation {
@@ -11,4 +12,10 @@ export interface SeedLocation {
     mobs: SeedMobData[];
 }
 
-export const seed49Data: SeedLocation[] = data;
+export const seed49Data: SeedLocation[] = data.map(l => ({
+    ...l,
+    mobs: l.mobs.map(m => ({
+        ...m,
+        img: encodeURI(`/images/seed/49/${m.name}.png`)
+    })),
+}));
