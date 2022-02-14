a566c2581b428e04f493b2a8b6d79ba11d1b77da
Sangbin Lee <leesangbin@outlook.com>
2022-02-15 00:47:29 +0100

    add default api url

--- src/tools/config.ts
+++ src/tools/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.mapleutils.com/api';
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://mapleutils.com';
