7723b3531885d0cadb23b5c6c9887f4f723cad98
Sangbin Lee <leesangbin@outlook.com>
2022-02-16 22:47:26 +0100

    add play/pause for music item

--- src/components/music-player/MusicItem.tsx
+++ src/components/music-player/MusicItem.tsx
@@ -2,6 +2,7 @@ import { Avatar, ListItemButton, ListItemIcon, ListItemText, Typography } from '
 import { useMusicPlayerContext } from '@components/music-player/MusicPlayerContext';
 import { useStore } from '@stores/StoreContext';
 import { useMemo, useRef } from 'react';
+import { PauseRounded, PlayArrowRounded } from '@mui/icons-material';
 
 interface MusicItemProps {
     src: string;
@@ -13,16 +14,22 @@ interface MusicItemProps {
 const MusicItem = (props: MusicItemProps) => {
     const { label, src, icon } = props;
     const { app } = useStore();
-    const { setTrack, onClip } = useMusicPlayerContext();
+    const { setTrack, onClip, track, state, setState } = useMusicPlayerContext();
     const buttonRef = useRef<HTMLDivElement>(null);
+    const isSelected = useMemo(() => track?.name === label, [track, label]);
+    const isPlaying = useMemo(() => state === 'playing', [state]);
 
     const togglePlay = () => {
         if (buttonRef.current) {
             buttonRef.current.blur();
         }
-        setTrack({ name: label, src, coverImg: icon });
-        if (app.preference.seed['24'].autoClip) {
-            onClip(label);
+        if (isSelected) {
+            setState(isPlaying ? 'paused' : 'playing');
+        } else {
+            setTrack({ name: label, src, coverImg: icon });
+            if (app.preference.seed['24'].autoClip) {
+                onClip(label);
+            }
         }
     };
 
@@ -36,8 +43,9 @@ const MusicItem = (props: MusicItemProps) => {
                     {label}
                 </Typography>
             </ListItemText>
+            {isSelected && isPlaying ? <PauseRounded color={'action'} /> : <PlayArrowRounded color={'action'} />}
         </ListItemButton>
-    ), []);
+    ), [isPlaying, isSelected]);
 };
 
 export default MusicItem;
