446c781c8e595abee06492900548b9c78e985d81
Sangbin Lee <leesangbin@outlook.com>
2022-02-16 01:44:27 +0100

    add links to github in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import { CharacterCard, TitleCard } from '@components/card';
 import { Button, Card, CardContent, Grid, Typography } from '@mui/material';
 import { Link } from '@components/link';
 import { KakaoTalkIcon } from '@components/icons';
-import { Box } from '@mui/system';
 import { Comments } from '@components/comments';
+import { GitHub } from '@mui/icons-material';
 
 const Home: NextPage = () => {
     return (
@@ -12,50 +12,87 @@ const Home: NextPage = () => {
             <TitleCard title={'환영합니다!'} />
             <Card variant={'outlined'}>
                 <CardContent>
-                    <Grid container alignItems={'center'} marginBottom={theme => theme.spacing(1)}>
-                        <Grid item>
-                            <Typography variant={'h5'} component={'span'}>
-                                각종 문의 및 버그 제보는
-                            </Typography>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12} md={6}>
+                            <Grid container alignItems={'center'} marginBottom={theme => theme.spacing(1)}>
+                                <Grid item>
+                                    <Typography variant={'h5'} component={'span'}>
+                                        각종 문의 및 버그 제보는
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Button startIcon={<KakaoTalkIcon />}
+                                            noLinkStyle
+                                            sx={theme => ({ margin: theme.spacing(1) })}
+                                            color={'inherit'}
+                                            rel={'noreferrer noopener'}
+                                            href={'https://open.kakao.com/o/sDbLVFoc'}
+                                            component={Link}
+                                            target={'_blank'}
+                                            size={'medium'}>
+                                        크로아 상빈
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant={'h5'} component={'span'}>
+                                        또는 댓글로 편하게 해주세요 😊
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12} />
+                                <Grid item>
+                                    <Button startIcon={<GitHub />}
+                                            noLinkStyle
+                                            sx={theme => ({ margin: theme.spacing(1) })}
+                                            color={'inherit'}
+                                            rel={'noreferrer noopener'}
+                                            href={'https://github.com/leesangb/mapleutils-web'}
+                                            component={Link}
+                                            target={'_blank'}
+                                            size={'medium'}>
+                                        GitHub
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant={'h5'}>이슈 또는 PR도 환영입니다!</Typography>
+                                </Grid>
+                                <Grid item xs={12} />
+                                <Grid item><Button startIcon={<GitHub />}
+                                                   noLinkStyle
+                                                   sx={theme => ({ margin: theme.spacing(1) })}
+                                                   color={'inherit'}
+                                                   rel={'noreferrer noopener'}
+                                                   href={'https://github.com/leesangb/mapleutils-web/releases'}
+                                                   component={Link}
+                                                   target={'_blank'}
+                                                   size={'medium'}>
+                                    릴리즈 노트 바로가기
+                                </Button>
+                                </Grid>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Button startIcon={<KakaoTalkIcon />}
-                                    noLinkStyle
-                                    sx={theme => ({ margin: theme.spacing(1) })}
-                                    color={'inherit'}
-                                    rel={'noreferrer noopener'}
-                                    href={'https://open.kakao.com/o/sDbLVFoc'}
-                                    component={Link}
-                                    size={'medium'}>
-                                크로아 상빈
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant={'h5'} component={'span'}>
-                                또는 댓글로 편하게 해주세요 😊
-                            </Typography>
 
+                        <Grid item xs={12} md={6}>
+                            <Grid container spacing={1} alignItems={'center'}>
+                                <Grid item xs={6}>
+                                    <CharacterCard name={'상빈'}
+                                                   job={'듀블'}
+                                                   server={'크로아'} />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <CharacterCard name={'Lilly'}
+                                                   job={'호영'}
+                                                   server={'크로아'} />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant={'h5'} align={'center'} component={'div'} fontWeight={'medium'}>
+                                        ↑ 인게임 문의는 여기로 ↑
+                                    </Typography>
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
-                    <Box>
-                        <Grid container spacing={1} alignItems={'center'}>
-                            <Grid item xs={5} sm={4} md={3} lg={2}>
-                                <CharacterCard name={'상빈'}
-                                               job={'듀블'}
-                                               server={'크로아'} />
-                            </Grid>
-                            <Grid item xs={5} sm={4} md={3} lg={2}>
-                                <CharacterCard name={'Lilly'}
-                                               job={'호영'}
-                                               server={'크로아'} />
-                            </Grid>
-                            <Grid item xs={2} sm={4}>
-                                <Typography variant={'h5'} component={'div'} fontWeight={'medium'}>
-                                    👈 인게임 문의는 여기로
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Box>
+
+
                 </CardContent>
             </Card>
             <Comments title={'피드백'} pageKey={'feedbacks'} defaultOpen />
