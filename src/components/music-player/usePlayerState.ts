a0833cb8aeb3bdb8b10c853ab47fd1d4a257d067
Sangbin Lee <leesangbin@outlook.com>
2022-02-27 11:05:44 +0100

    extract playerState logic in hooks

---
+++ src/components/music-player/usePlayerState.ts
@@ -0,0 +1,66 @@
+import { useReducer } from 'react';
+import { TrackInfo } from '@components/music-player/TrackInfo';
+
+
+interface PlayerState {
+    track: TrackInfo | null;
+    audio: HTMLAudioElement | null;
+    state: 'playing' | 'paused' | 'stopped';
+    volume: number;
+    time: number;
+    duration: number;
+}
+
+const buildPlayerState = (volume: number = 0): PlayerState => ({
+    track: null,
+    audio: null,
+    state: 'stopped',
+    volume,
+    time: 0,
+    duration: 0,
+});
+
+
+const playerReducer = (state: PlayerState, action: { key: keyof PlayerState, value: any }): PlayerState => {
+    return {
+        ...state,
+        [action.key]: action.value,
+    };
+};
+
+
+export const usePlayerState = () => {
+    const [playerState, dispatchPlayer] = useReducer(playerReducer, buildPlayerState());
+
+    const setVolume = (volume: number) => dispatchPlayer({ key: 'volume', value: volume });
+    const setTime = (time: number) => {
+        if (playerState.audio) {
+            playerState.audio.currentTime = time;
+            dispatchPlayer({ key: 'time', value: time });
+        }
+    };
+
+    const setState = (state: 'playing' | 'paused' | 'stopped') => {
+        dispatchPlayer({ key: 'state', value: state });
+    };
+
+    const setTrack = (track: TrackInfo) => dispatchPlayer({ key: 'track', value: track });
+
+    const setDuration = (duration: number) => {
+        dispatchPlayer({ key: 'duration', value: duration });
+    };
+
+    const setAudio = (audio: HTMLAudioElement) => {
+        dispatchPlayer({ key: 'audio', value: audio });
+    };
+
+    return {
+        playerState,
+        setVolume,
+        setTime,
+        setState,
+        setTrack,
+        setDuration,
+        setAudio,
+    };
+};
