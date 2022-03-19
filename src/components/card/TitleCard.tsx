71161263bed084655201e167793ddafa2ddf22fd
Sangbin Lee <leesangbin@outlook.com>
2022-03-19 12:12:10 +0100

    add page view and users in TitleCard

--- src/components/card/TitleCard.tsx
+++ src/components/card/TitleCard.tsx
@@ -1,12 +1,20 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import { useMemo } from 'react';
+import { formatNumberComma } from '@tools/string';
+import { useRouter } from 'next/router';
+import useFetch from '@hooks/useFetch';
+import { AnalyticsData } from '@api/analytics';
 
 interface TitleCardProps {
     title: string;
     marginRight?: number;
+    showAnalytics?: boolean;
 }
 
 const TitleCard = (props: TitleCardProps) => {
+    const { pathname } = useRouter();
+    const { data } = useFetch<AnalyticsData>(`/api/page-views?slug=${pathname}`);
+
     return useMemo(() => (
         <Card elevation={0}
               variant={'outlined'}
@@ -16,13 +24,18 @@ const TitleCard = (props: TitleCardProps) => {
                       ? theme.spacing(props.marginRight)
                       : undefined,
               })}>
-            <CardContent>
+            <CardContent sx={{ display: 'flex' }}>
                 <Typography variant={'h1'}>
                     {props.title}
                 </Typography>
+                {props.showAnalytics && data &&
+                    <Typography alignSelf={'center'} marginLeft={'auto'} align={'right'} variant={'caption'}>
+                        조회수 : {formatNumberComma(data.views)}
+                        <br />
+                        방문수 : {formatNumberComma(data.users)}</Typography>}
             </CardContent>
         </Card>
-    ), [props]);
+    ), [props, data]);
 };
 
 export default TitleCard;
