324ef705190903093505dc3139046ee7a4849c51
Sangbin Lee <leesangbin@outlook.com>
2022-03-19 12:14:57 +0100

    rename label

--- src/components/card/TitleCard.tsx
+++ src/components/card/TitleCard.tsx
@@ -30,9 +30,9 @@ const TitleCard = (props: TitleCardProps) => {
                 </Typography>
                 {props.showAnalytics && data &&
                     <Typography alignSelf={'center'} marginLeft={'auto'} align={'right'} variant={'caption'}>
-                        조회수 : {formatNumberComma(data.views)}
+                        조회 : {formatNumberComma(data.views)}
                         <br />
-                        방문수 : {formatNumberComma(data.users)}</Typography>}
+                        방문 : {formatNumberComma(data.users)}</Typography>}
             </CardContent>
         </Card>
     ), [props, data]);
