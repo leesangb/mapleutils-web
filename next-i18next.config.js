c5dee0a0166af6fe089f9c2701e90a45db571461
Sangbin Lee <leesangbin@outlook.com>
2022-05-01 12:02:28 +0200

    do not detect locale automatically

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,7 @@ module.exports = {
     i18n: {
         defaultLocale: 'kr',
         locales: ['kr', 'en'],
+        localeDetection: false,
     },
     reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
