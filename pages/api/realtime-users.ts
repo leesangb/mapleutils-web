0b6cda8a279407fcdc40e746fd4bdbddc5c03bcc
Sangbin Lee <leesangbin@outlook.com>
2022-05-08 20:05:03 +0200

    fix eslint

--- pages/api/realtime-users.ts
+++ pages/api/realtime-users.ts
@@ -1,12 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getRealTimeData } from '@api/analytics';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const slug = req.query.slug as string;
+const realtimeUsers = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const analytics = await getRealTimeData();
         return res.status(200).json(analytics);
     } catch (e: any) {
         return res.status(500).json({ error: e.message });
     }
-}
+};
+
+export default realtimeUsers;
