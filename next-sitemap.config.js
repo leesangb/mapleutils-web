4b9d5470985c58a6a0c9a9db2c1cf30baac5ff63
Sangbin Lee <leesangbin@outlook.com>
2022-05-10 22:16:53 +0200

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,11 @@ module.exports = {
     siteUrl: 'https://mapleutils.com',
     generateRobotsTxt: true,
     exclude: [
+        '/en/help/*',
         '/help/*',
+        '/en/api/*',
         '/api/*',
+        '/en/404',
+        '/404',
     ],
 };
