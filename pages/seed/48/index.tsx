db0948db98ef4296eb9b2657c41f34e24f71e015
Sangbin Lee <leesangbin@outlook.com>
2022-05-24 23:39:52 +0200

    add 48 map visual guide

--- pages/seed/48/index.tsx
+++ pages/seed/48/index.tsx
@@ -1,11 +1,12 @@
 import { TitleCard } from '@components/card';
-import { Card, CardContent } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
 import { VideoCapture } from '@components/video-capture';
 import { Comments } from '@components/comments';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import { Seo } from '@components/seo';
 import useI18nSeoProps from '@components/seo/useI18nSeoProps';
+import NextImage from 'next/image';
 
 
 const Seed48 = () => {
@@ -15,9 +16,15 @@ const Seed48 = () => {
         <>
             <Seo {...seoProps} image={'/images/48.png'} />
             <TitleCard title={t('title', { ns: 'seed48' })} />
-            <Card variant={'outlined'}>
+            <Card variant={'outlined'} sx={theme => ({ marginBottom: theme.spacing(1) })}>
                 <CardContent>
                     <VideoCapture />
+                </CardContent>
+            </Card>
+            <Card variant={'outlined'}>
+                <CardContent>
+                    <Typography variant={'h3'}>{t('mapGuide', { ns: 'seed48' })}</Typography>
+                    <NextImage src={'/images/seed/48.png'} width={3760} height={1270} />
                 </CardContent>
             </Card>
             <Comments pageKey={'seed48'} />
