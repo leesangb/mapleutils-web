59e4557fad856b27e6875cea18bf824f108564e5
Sangbin Lee <leesangbin@outlook.com>
2022-09-02 10:49:55 +0900

    add seed 24 data gms

--- data/seed/24/index.ts
+++ data/seed/24/index.ts
@@ -1,4 +1,6 @@
 import { TrackInfo } from '@components/seed/24/music-player';
 import data from './data.json';
+import dataGMS from './data.gms.json';
 
 export const seed24AudioData: TrackInfo[] = data;
+export const seed24AudioDataGMS: TrackInfo[] = dataGMS;
