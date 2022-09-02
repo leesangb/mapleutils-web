be05fff2c07bc216d28c001eafef4005e9004bed
Sangbin Lee <leesangbin@outlook.com>
2022-09-02 10:50:48 +0900

    use gms hint data in seed 24

--- pages/seed/24/index.tsx
+++ pages/seed/24/index.tsx
@@ -17,14 +17,14 @@ import { MusicPlayer, TrackInfo } from '@components/seed/24/music-player';
 import { Seo } from '@components/seo';
 import { I18nTitleCard } from '@components/card';
 import useWindowDimensions from '@hooks/useWindowDimensions';
-import { useCallback, useEffect } from 'react';
-import { isHangulMatching } from '@tools/string';
+import { useCallback } from 'react';
+import { isHangulMatching, isMatching } from '@tools/string';
 import { useTheme } from '@mui/system';
 import { TabContext, TabPanel } from '@mui/lab';
 import useCopy from '@hooks/useCopy';
 import { useTranslation } from 'next-i18next';
 import { TOptions } from 'i18next';
-import { seed24AudioData } from '@data/seed/24';
+import { seed24AudioData, seed24AudioDataGMS } from '@data/seed/24';
 import VirtualizedFixedList from '@components/virtualized-list/VirtualizedFixedList';
 import { Comments } from '@components/comments';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -39,11 +39,14 @@ const MEDIUM_SCREEN_ROW_SIZE = 60;
 
 const seed24Translation: TOptions = { ns: 'seed24' };
 
-const Seed24 = () => {
+interface Seed24Props {
+    data: TrackInfo[];
+}
+
+const Seed24 = ({ data }: Seed24Props) => {
     const { height } = useWindowDimensions();
     const seoProps = useI18nSeoProps('seed24');
     const { t, i18n } = useTranslation();
-    const isKMS = i18n.resolvedLanguage === Locales.Korean;
     const { copy, CopySnackbar } = useCopy();
     const theme = useTheme();
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
@@ -51,7 +54,7 @@ const Seed24 = () => {
 
     const rowRenderer = useCallback(
         (item: TrackInfo) => (
-            <Tooltip arrow placement={'top'} title={<Typography>{item.name} 복사하기</Typography>}>
+            <Tooltip arrow placement={'top'} title={<Typography>{t('copyItem', { text: item.name })}</Typography>}>
                 <ListItem button onClick={() => copy(item.name)}>
                     <Grid container spacing={1} alignItems={'center'}>
                         <Grid item xs={3} sm={2} md={1}>
@@ -74,18 +77,14 @@ const Seed24 = () => {
         [],
     );
 
-    const searchFilter = useCallback((item: TrackInfo, pattern: string) => isHangulMatching(pattern, item.name, item.hint), []);
+    const searchFilter = useCallback((item: TrackInfo, pattern: string) => i18n.resolvedLanguage === Locales.Korean
+        ? isHangulMatching(pattern, item.name, item.hint)
+        : isMatching(pattern, item.name, item.hint), [i18n.resolvedLanguage]);
 
 
     const handleChangeTab = (_: any, value: string) => {
         onChangeTab(value);
     };
-
-    useEffect(() => {
-        if (i18n.resolvedLanguage !== Locales.Korean) {
-            onChangeTab('bgm');
-        }
-    }, [i18n.resolvedLanguage, onChangeTab]);
 
     return (
         <>
@@ -96,14 +95,14 @@ const Seed24 = () => {
                 <Paper variant={'outlined'}>
                     <Tabs value={tab} onChange={handleChangeTab} centered>
                         <Tab value={'bgm'} label={t('bgm', seed24Translation)} />
-                        <Tab disabled={!isKMS} value={'hint'} label={t('hint', seed24Translation)} />
+                        <Tab value={'hint'} label={t('hint', seed24Translation)} />
                     </Tabs>
                 </Paper>
 
                 <TabPanel sx={{ padding: `${theme.spacing(1)} 0` }} value={'bgm'}>
                     <Card elevation={0} variant={'outlined'} component={'section'}>
                         <CardContent>
-                            <MusicPlayer tracks={seed24AudioData} />
+                            <MusicPlayer tracks={data} />
                         </CardContent>
                     </Card>
                 </TabPanel>
@@ -111,13 +110,13 @@ const Seed24 = () => {
                 <TabPanel sx={{ padding: `${theme.spacing(1)} 0` }} value={'hint'}>
                     <Card elevation={0} variant={'outlined'} component={'section'}>
                         <CardContent>
-                            <VirtualizedFixedList items={seed24AudioData}
+                            <VirtualizedFixedList items={data}
                                                   height={height - SCREEN_HEIGHT_OFFSET}
                                                   width={'100%'}
                                                   rowSize={smDown ? SMALL_SCREEN_ROW_SIZE : MEDIUM_SCREEN_ROW_SIZE}
                                                   divider
                                                   searchFilter={searchFilter}
-                                                  placeholder={'힌트 검색 (예: 리스항구, 푸른빛, ㅍㄹㅂ, ...) [Ctrl] + [F] 또는 [F3]으로 포커싱, 초성 검색 ✅'}
+                                                  placeholder={t('searchPlaceholder', { ns: 'seed24' })}
                                                   rowRenderer={rowRenderer} />
                         </CardContent>
                     </Card>
@@ -145,6 +144,9 @@ const Seed24 = () => {
 export const getStaticProps = async ({ locale }: { locale: string }) => {
     return {
         props: {
+            data: locale === Locales.Korean
+                ? seed24AudioData
+                : seed24AudioDataGMS,
             ...(await serverSideTranslations(locale, ['common', 'seed24'])),
         },
     };
