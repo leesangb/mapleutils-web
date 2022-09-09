34c60deefa5aab1596d6ed471713b269f005b130
Sangbin Lee <leesangbin@outlook.com>
2022-09-09 21:16:17 +0900

    add hook for seed 49 favorite filter

---
+++ src/components/seed/49/useSeed49Favorite.tsx
@@ -0,0 +1,23 @@
+import { useCallback, useEffect, useState } from 'react';
+import { LocalStorageHelper, LocalStorageKey } from '@tools/localStorageHelper';
+
+const useSeed49Favorite = () => {
+    const [showOnlyFavorite, setShowOnlyFavorite] = useState<boolean>(false);
+
+    const onChangeShowOnlyFavorite = useCallback((show: boolean) => {
+        setShowOnlyFavorite(show);
+        LocalStorageHelper.save(LocalStorageKey.SEED_49_SHOW_FILTER, show);
+    }, []);
+
+    useEffect(() => {
+        const showFilter = LocalStorageHelper.load<boolean>(LocalStorageKey.SEED_49_SHOW_FILTER);
+        setShowOnlyFavorite(showFilter);
+    }, []);
+
+    return {
+        showOnlyFavorite,
+        onChangeShowOnlyFavorite,
+    };
+};
+
+export default useSeed49Favorite;
