3f528f650cefcc36b656c49acd5a79894473dd74
Sangbin Lee <leesangbin@outlook.com>
2022-10-15 11:22:20 +0900

    add wachan api

---
+++ pages/api/wachan.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getWachanFarms } from '@api/wachan';
+
+const getFarms = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        if (!req.query.name || typeof req.query.name !== 'string') {
+            return;
+        }
+        const farms = await getWachanFarms(req.query.name);
+        res.status(200).json(farms);
+    } catch (e: any) {
+        return res.status(500).json({ error: e.message });
+    }
+};
+
+export default getFarms;
