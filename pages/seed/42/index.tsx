a21fac228f2051a0b9de7c530184a47bcc3909cc
Sangbin Lee <leesangbin@outlook.com>
2023-01-15 23:29:06 +0900

    add seed 42 options

--- pages/seed/42/index.tsx
+++ pages/seed/42/index.tsx
@@ -2,15 +2,29 @@ import { useTranslation } from 'next-i18next';
 import useI18nSeoProps from '@components/seo/useI18nSeoProps';
 import { Seo } from '@components/seo';
 import { I18nTitleCard } from '@components/card';
-import { Box, Card, CardContent, Slider, styled } from '@mui/material';
+import {
+    Box,
+    Card,
+    CardContent,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Grid,
+    Radio,
+    RadioGroup,
+    Slider,
+    styled,
+    Typography,
+} from '@mui/material';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Comments } from '@components/comments';
 import { useState } from 'react';
+import NextImage from 'next/image';
 
 interface Seed42Props {
 }
 
-const Image = styled('img')`
+const LayerImage = styled('img')`
   position: absolute;
   width: 100%;
   top: 0;
@@ -18,9 +32,10 @@ const Image = styled('img')`
 `;
 
 const Seed42 = ({}: Seed42Props) => {
-    const { t, i18n } = useTranslation();
+    const { t } = useTranslation('seed42');
     const seoProps = useI18nSeoProps('seed42');
     const [opacity, setOpacity] = useState<number>(50);
+    const [route, setRoute] = useState<string>('none');
     return (
         <>
             <Seo {...seoProps} />
@@ -28,20 +43,54 @@ const Seed42 = ({}: Seed42Props) => {
 
             <Card elevation={0} variant={'outlined'} component={'section'}>
                 <CardContent>
-                    <img src={'/images/seed/42/help.png'} alt={'help'} />
+                    <Typography variant={'h3'} gutterBottom>
+                        {t('fakePlatforms')}
+                    </Typography>
                     <Box sx={{
                         position: 'relative',
                     }}>
-                        <Image sx={{ position: 'relative', top: 0, left: 0 }}
-                               src={'/images/seed/42/background.png'}
-                               draggable={false} />
-                        <Image src={'/images/seed/42/route.png'} draggable={false} />
-                        <Image src={'/images/seed/42/trap.png'} sx={{ opacity: opacity / 100 }} draggable={false} />
+                        <NextImage style={{ position: 'relative', top: 0, left: 0 }} draggable={false}
+                                   src={'/images/seed/42/background.png'} width={1704} height={1000} />
+                        <LayerImage src={'/images/seed/42/route.png'} draggable={false} />
+                        <LayerImage src={'/images/seed/42/trap.png'} sx={{ opacity: opacity / 100 }}
+                                    draggable={false} />
+                        {route === 'route1' && <LayerImage src={'/images/seed/42/path1.png'} draggable={false} />}
+                        {route === 'route2' && <LayerImage src={'/images/seed/42/path2.png'} draggable={false} />}
                     </Box>
-                    <Slider value={opacity}
-                            onChange={(e, v) => setOpacity(v as number)}
-                            min={0}
-                            max={100} />
+                    <Grid container spacing={1} alignItems={'center'}
+                          justifyContent={'center'}>
+                        <Grid item xs={12} sm={4} sx={{ textAlign: 'center' }}>
+                            <img style={{ maxWidth: 200 }} src={'/images/seed/42/help.png'} alt={'help'}
+                                 draggable={false} />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Typography align={'center'}>
+                                {t('fakePlatformsOpacity')}
+                            </Typography>
+                            <Slider value={opacity}
+                                    onChange={(e, v) => setOpacity(v as number)}
+                                    min={0}
+                                    max={100} />
+                        </Grid>
+                        <Grid item xs={12} sm={4} sx={{ textAlign: 'center' }}>
+                            <FormControl>
+                                <FormLabel id='route-radio'>{t('path')}</FormLabel>
+                                <RadioGroup row aria-labelledby='route-radio' name='route-radio-group'
+                                            value={route}
+                                            onChange={e => setRoute((e.target as HTMLInputElement).value)}>
+                                    <FormControlLabel value='none' control={<Radio />} label={t('none')} />
+                                    <FormControlLabel value='route1' control={<Radio />} label={t('path1')} />
+                                    <FormControlLabel value='route2' control={<Radio />} label={t('path2')} />
+                                </RadioGroup>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+
+                    <Typography variant={'h3'} gutterBottom>
+                        {t('lastPart')}
+                    </Typography>
+
+                    <NextImage src={'/images/seed/42/end.png'} alt={''} width={852} height={370} />
 
                 </CardContent>
             </Card>
