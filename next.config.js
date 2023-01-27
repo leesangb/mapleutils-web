7e6ef085c4458271a92c835ab45e316ef2af5007
Sangbin Lee <leesangbin@outlook.com>
2023-01-27 22:40:48 +0900

    add opencv

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config');
+const webpack = require('webpack');
 
 module.exports = {
     reactStrictMode: true,
     i18n,
+    webpack5: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, path: false, crypto: false };
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
+        );
+        return config;
+    },
 };
