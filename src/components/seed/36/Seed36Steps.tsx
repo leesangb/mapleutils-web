beabf5fba316e5865fca5b433777a2d5a024b8f1
Sangbin Lee <leesangbin@outlook.com>
2023-02-17 22:41:50 +0900

    add horizontal mode

--- src/components/seed/36/Seed36Steps.tsx
+++ src/components/seed/36/Seed36Steps.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from 'next-i18next';
-import { Box, Button, CardActionArea, Grid, Hidden, Typography } from '@mui/material';
-import { ReplayRounded } from '@mui/icons-material';
+import { Box, Button, CardActionArea, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { seed36Data } from '@data/seed/36';
 import { useState } from 'react';
 import { styled } from '@mui/system';
 import NextImage from 'next/image';
+import { AlignHorizontalLeftRounded, AlignVerticalTopRounded, ReplayRounded } from '@mui/icons-material';
 
 type Step = 0 | 1 | 2 | 3 | undefined;
 type Steps = [Step, Step, Step, Step, Step, Step, Step, Step]
@@ -24,71 +24,86 @@ const Seed36Steps = () => {
     const onChangeStep = (step: Step, index: number) => {
         setSteps(steps => steps.map((s, i) => i === index ? step : s) as Steps);
     };
+
+    const [alignment, setAlignment] = useState<string>('vertical');
+    const isVertical = alignment === 'vertical';
+
+    const handleAlignment = (
+        event: React.MouseEvent<HTMLElement>,
+        newAlignment: string | null,
+    ) => {
+        if (!newAlignment) {
+            return;
+        }
+        setAlignment(newAlignment);
+    };
+
     return (
-        <>
-            <Grid container>
-                <Hidden smUp>
-                    <Grid item xs={12}>
-                        <Button sx={(theme) => ({ marginBottom: theme.spacing(1), float: 'right' })}
-                                variant={'contained'} disableElevation
-                                onClick={resetSteps}
-                                startIcon={<ReplayRounded />}>
-                            {t('reset')}
-                        </Button>
-                    </Grid>
-                </Hidden>
-                {steps.map((s, i) => (
-                    <Grid item xs={12} key={`step-${i}`}>
-                        <Grid container spacing={1} alignItems='center'>
-                            <Hidden smDown>
-                                <Grid item sm={2}>
-                                    <Typography align={'center'} variant={'h3'}>
-                                        {t('step', { ns: 'seed36', step: i + 1 })}
-                                    </Typography>
-                                </Grid>
-                            </Hidden>
+        <section>
+            <Box display={'flex'} alignItems={'center'} marginBottom={1}>
+                <ToggleButtonGroup size={'small'}
+                                   value={alignment}
+                                   exclusive
+                                   onChange={handleAlignment}>
+                    <ToggleButton value='vertical'>
+                        <AlignVerticalTopRounded /> {t('vertical', { ns: 'seed36' })}
+                    </ToggleButton>
+                    <ToggleButton value='horizontal'>
+                        <AlignHorizontalLeftRounded /> {t('horizontal', { ns: 'seed36' })}
+                    </ToggleButton>
+                </ToggleButtonGroup>
+                <Button sx={(theme) => ({ marginLeft: 'auto', marginBottom: theme.spacing(1) })}
+                        variant={'contained'} disableElevation
+                        onClick={resetSteps}
+                        startIcon={<ReplayRounded />}>
+                    {t('reset')}
+                </Button>
+            </Box>
+            <Box display={'flex'} flexDirection={isVertical ? 'column' : 'row'}>
+                {steps.map((step, i) =>
+                    <Box key={`step${i}`}
+                         display={'flex'}
+                         alignItems={isVertical ? 'center' : undefined}
+                         width={'100%'}
+                         margin={isVertical ? undefined : '4px'}
+                         flexDirection={isVertical ? 'row' : 'column'}>
+                        <Typography variant={'h3'} align={'center'} margin={'4px'} minWidth={'fit-content'}>
+                            {t('step', { ns: 'seed36', step: i + 1 })}
+                        </Typography>
+                        <Box display={'flex'} flexBasis={'auto'} flexGrow={1}
+                             flexDirection={isVertical ? 'row' : 'column'}>
                             {
-                                seed36Data.map((mob, choice) => (
-                                    <Grid item xs={3} sm={2} key={`${i}-${choice}`}>
-                                        <CardActionArea sx={theme => ({
-                                            borderRadius: theme.shape.borderRadius,
-                                            marginBottom: theme.spacing(2),
-                                            height: theme.spacing(9),
-                                            backgroundColor: steps[i] === choice
-                                                ? theme.palette.primary.light
-                                                : theme.palette.background.default,
-                                            transition: '0.3s',
-                                        })}
-                                                        onClick={() => onChangeStep(choice as Step, i)}>
-                                            <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                                                <StyledImage src={mob.icon}
-                                                             alt={mob.name}
-                                                             width={mob.width}
-                                                             height={mob.height} />
-                                            </Box>
-                                        </CardActionArea>
-                                    </Grid>
-                                ))
+                                seed36Data.map((mob, choice) =>
+                                    <CardActionArea key={mob.name + i}
+                                                    sx={theme => ({
+                                                        borderRadius: theme.shape.borderRadius,
+                                                        ...(isVertical ? {
+                                                            marginLeft: theme.spacing(1),
+                                                        } : {
+                                                            marginLeft: theme.spacing(0.5),
+                                                            marginRight: theme.spacing(0.5),
+                                                        }),
+                                                        marginBottom: theme.spacing(1),
+                                                        height: theme.spacing(9),
+                                                        backgroundColor: steps[i] === choice
+                                                            ? theme.palette.primary.light
+                                                            : theme.palette.background.default,
+                                                        transition: '0.3s',
+                                                    })}
+                                                    onClick={() => onChangeStep(choice as Step, i)}>
+                                        <Box display={'flex'} justifyContent={'center'}
+                                             alignItems={'center'}>
+                                            <StyledImage src={mob.icon}
+                                                         alt={mob.name}
+                                                         width={mob.width}
+                                                         height={mob.height} />
+                                        </Box>
+                                    </CardActionArea>)
                             }
-                            {
-                                i === 0 && (
-                                    <Hidden smDown>
-                                        <Grid item sm={2}>
-                                            <Box display={'flex'} justifyContent={'right'}>
-                                                <Button variant={'contained'} disableElevation onClick={resetSteps}
-                                                        startIcon={<ReplayRounded />}>
-                                                    {t('reset')}
-                                                </Button>
-                                            </Box>
-                                        </Grid>
-                                    </Hidden>
-                                )
-                            }
-                        </Grid>
-                    </Grid>
-                ))}
-            </Grid>
-        </>
+                        </Box>
+                    </Box>)}
+            </Box>
+        </section>
     );
 };
 
