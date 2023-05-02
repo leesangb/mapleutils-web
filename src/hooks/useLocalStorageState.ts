9fe375c4abcd537dbc6949d0e6a3ba11ffaca96a
Sangbin Lee <leesangbin@outlook.com>
2023-05-02 23:18:09 +0900

    add useLocalStorageState hook

---
+++ src/hooks/useLocalStorageState.ts
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export const useLocalStorageState = <T>(key: string, defaultValue: T) => {
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        if (typeof window === 'undefined') {
+            return defaultValue;
+        }
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : defaultValue;
+        } catch (error) {
+            console.log(error);
+            return defaultValue;
+        }
+    });
+
+    const setValue = (value: T) => {
+        try {
+            const valueToStore =
+                value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            if (typeof window !== 'undefined') {
+                window.localStorage.setItem(key, JSON.stringify(valueToStore));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return [storedValue, setValue] as const;
+};
