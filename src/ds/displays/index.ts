44f2124483efc507a370062d475e9937f7deb2cc
Sangbin Lee <leesangbin@outlook.com>
2023-05-07 01:03:43 +0900

    add box component

--- src/ds/displays/index.ts
+++ src/ds/displays/index.ts
@@ -3,3 +3,4 @@
 export { Avatar } from './avatar/Avatar';
 export { Typography } from './typography/Typography';
 export { Link } from './link/Link';
+export { Box } from './box/Box';
--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -1,13 +1,5 @@
 import { Theme } from '@/ds/theme';
 
-type addPrefixToObject<T, P extends string> = {
-    [K in keyof T as K extends string ? `${P}${K}` : never]: T[K]
-}
-
-type TransientIgnoredProps = 'as' | 'children' | 'className' | 'style'
-
-type TransientProps<T> = addPrefixToObject<Omit<T, TransientIgnoredProps>, '$'>
-
 declare module 'styled-components' {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
     export interface DefaultTheme extends Theme {
