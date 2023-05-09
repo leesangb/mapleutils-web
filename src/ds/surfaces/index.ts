77bc078b2d38082cabf5257c473385c62808dadb
Sangbin Lee <leesangbin@outlook.com>
2023-05-10 02:32:10 +0900

    fix index

--- src/ds/surfaces/index.ts
+++ src/ds/surfaces/index.ts
@@ -4,3 +4,4 @@ export { Card } from './card/Card';
 export { AppBar } from './bar/AppBar';
 export { SideBar } from './bar/SideBar';
 export * from './accordion/Accordion';
+export * from './tab/Tab';
