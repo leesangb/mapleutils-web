57bb1dc1bd4d706f88fc58bcf9fa40dc6b7b6adf
Sangbin Lee <leesangbin@outlook.com>
2023-05-22 02:41:11 +0900

    add seed 47 page

--- app/[lng]/seed/(card)/47/page.tsx
+++ app/[lng]/seed/(card)/47/page.tsx
@@ -1,8 +1,24 @@
-const Seed47Page = () => {
+import { Typography } from '@/ds/displays';
+import { I18nPageProps } from '@/i18n/settings';
+import { useTranslation } from '@/i18n/server';
+import seed47 from '@/assets/images/seed/47.png';
+import NextImage from 'next/image';
+
+const Seed47Page = async ({ params }: I18nPageProps) => {
+    const { t } = await useTranslation(params.lng, 'seed47');
     return (
-        <div>
-            <h1>Seed 47 Page</h1>
-        </div>
+        <>
+            <Typography as={'h2'} style={{ marginBottom: '8px' }}>{t('map')}</Typography>
+            <Typography>{t('switchDescription')}</Typography>
+
+            <NextImage style={{
+                objectFit: 'scale-down',
+                maxWidth: '100%',
+                height: 'auto',
+                display: 'block',
+                margin: '8px auto',
+            }} src={seed47} alt={'seed 47'} />
+        </>
     );
 };
 
