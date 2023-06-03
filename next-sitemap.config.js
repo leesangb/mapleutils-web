088f97d19c0417582249e61b92564dc7df64da77
Sangbin Lee <leesangbin@outlook.com>
2023-06-04 01:37:28 +0900

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://mapleutils.com',
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    exclude: [
+        '/en/farm/*',
+        '/zh-TW/farm/*',
+    ],
+};
