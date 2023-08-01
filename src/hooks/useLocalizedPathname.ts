660274e5e6816a7b143c030062c0fd393e8ddba2
Sangbin Lee <leesangbin@outlook.com>
2023-08-01 20:44:02 +0900

    fix localized pathname

--- src/hooks/useLocalizedPathname.ts
+++ src/hooks/useLocalizedPathname.ts
@@ -2,7 +2,7 @@ import { usePathname } from 'next/navigation';
 import { Languages } from '@/i18n/settings';
 
 export const useLocalizedPathname = () => {
-    const pathname = usePathname();
+    const pathname = usePathname() || '';
     const [root, locale, ...rest] = pathname.split('/') as [string, Languages, ...string[]];
 
     return {
